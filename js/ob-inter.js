fbcde236f614519925e2e530e843ceeb10d7b68a
Developers <blacksk81@gmail.com>
2018-11-19 11:02:41 -0500

    Commit
    Version modificada

--- js/ob-inter.js
+++ js/ob-inter.js
@@ -1,45 +1,4 @@
-<h1>Observador de intersection de imagenes JS</h1>
-	<h2>by:luisbernal</h2>
-
-<p>Este pequeño scritp nos ayudara a la carga asincronica de imagenes el campo visual de la pantalla de esta manera las images que estan fuera del campo visual de la pantalla del usuario su carga se encontrara en espera hasta que sea visible para el usuario, pero para que la carga no sea tan grotesta y de golpe agregaremos un pequeño css para que la transcion mas suave entre images</p>
-
-
-<p>Paso uno Agregar el CSS</p>
-
-<pre>
-	.fade-in {
-      animation-name: fadeIn;
-      animation-duration: 1.3s;
-      animation-timing-function: cubic-bezier(0, 0, 0.4, 1);
-      animation-fill-mode: forwards;
-    }
-
-    @keyframes fadeIn {
-      from {
-        opacity: 0;
-      }
-
-      to {
-        opacity: 1;
-      }
-    }
-</pre>
-
-<p>Paso dos cambio de atributto a nuestra  etiqueta img cambiar scr por data-scr </p>
-
-<pre>
-
- src="cometanike.png
- <br>
- data-src="cometanike.png
-
-</pre>
-
-<p>Paso tres agregar el siguiente scritp a nuestra web al final</p>
-
-<pre>
-
-'use strict';
+ 'use strict';
  var images = document.querySelectorAll('img'),
      config = {
          rootMargin: '50px 0px',
@@ -82,9 +41,4 @@
 
  function applyImage(a, b) {
      a.classList.add('js-lazy-image--handled'), a.src = b, a.classList.add('fade-in')
- }	
-
-</pre>
-
-<p><b>ESTA FUNCION ESTA SOLO PARA LA CARGA DE IMAGENES</b></p>
-
+ }
